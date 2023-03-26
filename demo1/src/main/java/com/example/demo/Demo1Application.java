e0c81e9d16f69f6385713b2f8beca527e1563e33
Galler <1925227051@qq.com>
2023-03-26 19:50:41 +0800

    four commit

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.demos.controller.helloSpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Demo1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo1Application.class, args);
+//        引导类得到的是一个容器
+        ConfigurableApplicationContext context = SpringApplication.run(Demo1Application.class, args);
+        helloSpringBoot bean = context.getBean(helloSpringBoot.class);
+        System.out.println("bean=======>"+bean);
+        student bean1 = context.getBean(student.class);
+        System.out.println("bean1======>"+bean1);
     }
 
 }
