dfe7a0f4f3859619101a92c73976e34c1058f61b
Galler <1925227051@qq.com>
2023-04-23 21:21:51 +0800

    2023.04.23 21:21 commit

--- demo7/src/main/java/com/example/demo/Demo7Application.java
+++ demo7/src/main/java/com/example/demo/Demo7Application.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.demo.demos.mapper")
 public class Demo7Application {
 
     public static void main(String[] args) {
